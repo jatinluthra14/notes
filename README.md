10bb801cc15cfd90aa2126575b324e71e484ae0b
Anuj Garg <anujgargcse@gmail.com>
2017-12-14 00:20:11 +0530

    Revert "MongoDB port with mongoose (#9)" (#12)
    This reverts commit c52febd077d97471b6e4cb2c832634a87097faaa.

--- README.md
+++ README.md
@@ -2,53 +2,14 @@
 
 A node application to manage notes
 
-## Prerequisites
+## Getting Started :
 
-### Install [Nodejs](https://nodejs.org/en)
-
-Install Node.js for running the project.
-You can use nvm to install Node.js
-Here is the [Guide](https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04) for it.
-
-### Install [MongoDB](https://www.mongodb.com)
-
-Install MongoDB forstoring the notes.
-You can Install it using the following [Guide](https://www.howtoforge.com/tutorial/install-mongodb-on-ubuntu-16.04/}
-
-### Clone the Repo
-
-Clone the "notes" repo using git
+* Install npm, node
+* Clone this repo
+    
 ```
-git clone https://github.com/jboss-outreach/notes
+$ npm install
+$ node server.js
 ```
 
-## Run the Project
 
-### Start MongoDB Server
-
- Start the MongoDb server being root
-```
-mongod &
-```
-
- If you are not a root user, run
-```
-sudo mongod &
-```
-
- If you have less storage space, use
-```
-mongod --smallfiles &
-```
-
-### Install Node.js Dependencies
-
-Install the dependencies using npm
-```
-npm install
-```
-
-### Start the server
-```
-node server.js
-```
--- server.js
+++ server.js
@@ -3,49 +3,42 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
 
 const app = express();
-const mongoDB = 'mongodb://localhost:27017';
 
-mongoose.connect(mongoDB + '/notes'); //Initate the connection with notes db
-
-var note_Schema = mongoose.Schema({
-	task: { type: 'String', required: true },
-	done: { type: 'Boolean', required: false}
-});						//Create the Basic Schema for Collection
-
-var Note = mongoose.model('Notes', note_Schema); //Create a mongoose model for the desired Schema
+// Create database directory on local if not existing
+mkdirp("db", function (err){
+    if (err) return cb(err);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var notes = [];
 
-var notes = function(res){
-	Note.find({}, '-_id task done', function(error, data){
-        res.send(data);
-        });					//Return all the notes from collection
-}
+// read database file from local to create initial data
+fs.readFile('db/notes.json', function(err, data){
+  if(data){
+    let parsedData = JSON.parse(data);
+    if(typeof parsedData == "object"){
+      notes = parsedData;
+    }
+  }
+});
 
 // serve static content from under /public/ dir
 app.use('/', express.static(__dirname + "/public/"));
 
 app.post('/notes', function(req, res){
-
-  var new_note = new Note({
-	task: Object.values(req.body)[0],
-	done: Object.values(req.body)[1]
-  });						//Store the New note
-
-  new_note.save(function(err) {
-  if (err) throw err;
-  notes(res);
-  });
-
+  notes.push(req.body);
+  fs.writeFile('db/notes.json', JSON.stringify(notes), { flag: 'w' }, function(err){
+    if(err) throw err;
+  })
+  res.send(notes);
 });
 
 app.get('/notes', function(req, res){
-	notes(res);
+  res.send(notes);
 });
 
 app.listen(4000, function(){
