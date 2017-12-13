c52febd077d97471b6e4cb2c832634a87097faaa
Jatin Luthra <jatinluthra14@gmail.com>
2017-12-13 23:53:15 +0530

    MongoDB port with mongoose (#9)
    * Mongodb port with mongoose
    
    * Added Readme
    
    * Fixed typo in Readme

--- server.js
+++ server.js
@@ -3,42 +3,49 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+const mongoDB = 'mongodb://localhost:27017';
 
-// Create database directory on local if not existing
-mkdirp("db", function (err){
-    if (err) return cb(err);
-});
+mongoose.connect(mongoDB + '/notes'); //Initate the connection with notes db
+
+var note_Schema = mongoose.Schema({
+	task: { type: 'String', required: true },
+	done: { type: 'Boolean', required: false}
+});						//Create the Basic Schema for Collection
+
+var Note = mongoose.model('Notes', note_Schema); //Create a mongoose model for the desired Schema
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var notes = [];
 
-// read database file from local to create initial data
-fs.readFile('db/notes.json', function(err, data){
-  if(data){
-    let parsedData = JSON.parse(data);
-    if(typeof parsedData == "object"){
-      notes = parsedData;
-    }
-  }
-});
+var notes = function(res){
+	Note.find({}, '-_id task done', function(error, data){
+        res.send(data);
+        });					//Return all the notes from collection
+}
 
 // serve static content from under /public/ dir
 app.use('/', express.static(__dirname + "/public/"));
 
 app.post('/notes', function(req, res){
-  notes.push(req.body);
-  fs.writeFile('db/notes.json', JSON.stringify(notes), { flag: 'w' }, function(err){
-    if(err) throw err;
-  })
-  res.send(notes);
+
+  var new_note = new Note({
+	task: Object.values(req.body)[0],
+	done: Object.values(req.body)[1]
+  });						//Store the New note
+
+  new_note.save(function(err) {
+  if (err) throw err;
+  notes(res);
+  });
+
 });
 
 app.get('/notes', function(req, res){
-  res.send(notes);
+	notes(res);
 });
 
 app.listen(4000, function(){
